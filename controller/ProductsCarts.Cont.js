b96661f0200bc72e535523af01620243448e7852
Himanshu Jain <112754762+Himanshuj0311@users.noreply.github.com>
2025-02-12 12:51:01 +0530

    Update ProductsCarts.Cont.js

--- controller/ProductsCarts.Cont.js
+++ controller/ProductsCarts.Cont.js
@@ -60,7 +60,8 @@ const createOrUpdateCart = async (req, res) => {
 };
 
 const getCart = async (req, res) => {
-  const { userId } = req.quary; 
+  const { userId } = req.query;
+
 
   if (!userId) {
     return res.status(400).json({
